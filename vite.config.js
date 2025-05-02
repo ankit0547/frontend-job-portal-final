016824da7339155cdd4caabce11f89a1fe443f13
Ankit Sharma <ankit.sharma09547@gmail.com>
2025-05-02 19:21:54 +0530

    docker fixes

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,15 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// import { defineConfig, loadEnv } from "vite";
-
-// export default defineConfig(({ command, mode }) => {
-//   const env = loadEnv(mode, process.cwd(), "");
-//   return {
-//     define: {
-//       "process.env.YOUR_STRING_VARIABLE": JSON.stringify(
-//         env.YOUR_STRING_VARIABLE
-//       ),
-//       "process.env.YOUR_BOOLEAN_VARIABLE": env.YOUR_BOOLEAN_VARIABLE,
-//       // If you want to exposes all env variables, which is not recommended
-//       // 'process.env': env
-//     },
-//   };
-// });
-
 import { defineConfig } from "vite";
-// ...
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  // ...
-  define: {
-    "process.env": {},
+  plugins: [react()],
+  server: {
+    host: true, // or '0.0.0.0'
+    port: 4173, // default, change if needed
+    open: false, // auto-open browser
+  },
+  build: {
+    outDir: "dist", // where to output production build
+    sourcemap: false, // set to true for debugging production issues
   },
 });
